fa2bb29e6d1238b12200920ceca6b1096fdc9fbe
Svenar <11473832+svenar-nl@users.noreply.github.com>
2023-07-05 22:10:08 +0200

    Create index.php

---
+++ web/index.php
@@ -0,0 +1,34 @@
+<html lang="en">
+
+<head>
+    <title>NyaNya Games</title>
+    <meta charset="UTF-8" />
+    <style>
+        * {
+            margin: 0px;
+            padding: 0px;
+            font-family: Arial;
+            user-select: none;
+        }
+
+        body {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            background-color: #2D2D2D;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        h1 {
+            color: #FFF;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>MEOW!</h1>
+</body>
+
+</html>
